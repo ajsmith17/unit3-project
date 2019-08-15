ce6827a506e6d62c5156888a158d3d76b51d5107
Mason Griffin <masongriffin@Masons-MacBook-Pro.local>
2019-08-15 16:52:37 -0500

    server changes

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 //Dependencies
 //___________________
 const express = require('express');
-const methodOverride  = require('method-override');
 const mongoose = require ('mongoose');
 const app = express ();
 const db = mongoose.connection;
@@ -16,9 +15,6 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
-
-const citiesController = require('./controllers/cities.js');
-app.use('/cities', todosController);
 
 const PROJECT3_DB = process.env.PROJECT3_DB
 
