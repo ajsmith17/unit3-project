0ea4fd59b545ec5bb7e8126f19e50d034db1a84f
Mason Griffin <masongriffin@Masons-MacBook-Pro.local>
2019-08-15 16:46:00 -0500

    server

--- server.js
+++ server.js
@@ -1,20 +1,31 @@
+//___________________
+//Dependencies
+//___________________
 const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
+const methodOverride  = require('method-override');
+const mongoose = require ('mongoose');
+const app = express ();
+const db = mongoose.connection;
+//___________________
+//Port
+//___________________
+// Allow use of Heroku's port or your own local port, depending on the environment
+const PORT = process.env.PORT || 3000;
+
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
-
-const PROJECT3_DB = process.env.MONGODB_URI;
 
 const citiesController = require('./controllers/cities.js');
 app.use('/cities', todosController);
 
+const PROJECT3_DB = process.env.PROJECT3_DB
 
-mongoose.connect('mongodb://localhost:27017/cities', {useNewUrlParser:true});
-mongoose.connection.once('open', () => {
-  console.log('connected to mongoose...');
-})
+// Error / success
+db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
+db.on('connected', () => console.log('mongo connected: ', PROJECT3_DB));
+db.on('disconnected', () => console.log('mongo disconnected'));
 app.listen(3000, () => {
   console.log('listening');
 })
