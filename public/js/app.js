89a62df43f0accf93487046612a5e97b1c9d727e
Aaron <aaron.smith@uconn.edu>
2019-08-19 11:37:26 -0400

    add modal to index.html

--- public/js/app.js
+++ public/js/app.js
@@ -127,3 +127,23 @@ app.controller('MyController', ['$http', function($http){
 
   this.getCities();
   }]);
+
+$(() => {
+
+  const $openButton = $('.openModal');
+  const $modal = $('#modal');
+  const $close = $('#close');
+
+  const openModal = () => {
+    $modal.css('display', 'block');
+  };
+
+  const closeModal = () => {
+    $modal.css('display', 'none');
+  };
+
+  $openButton.on('click', openModal)
+  $close.on('click', closeModal)
+  setTimeout(openModal, 2000);
+
+})
